e0008217c0e6d8377f55e9f89c7441b9f0680730
smv5047 <smv5047@gmail.com>
2019-10-23 23:22:00 -0400

    updated app scaffolding and setting up redux in index

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Components
+import Cards from "./components/Cards"
+import Button from "./components/Button"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gotta Catch 'Em All!</h1>
+      <Button/>
+      <Cards/>
     </div>
   );
 }
